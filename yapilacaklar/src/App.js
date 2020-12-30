bdf63f7624918e98327bac5c9f9dda54fef5b9f8
musabal55 <musahoney@gmail.com>
2020-12-30 17:57:49 +0300

    git e gönder

---
+++ yapilacaklar/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import PostForm from './Components/PostForm'
+import Todolist from './Components/Todolist'
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <PostForm/>
+        <Todolist/>
+        
+      </div>
+    )
+  }
+}
+export default App;
---
+++ yapilacaklar/src/Components/Todolist.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react'
+import axios from 'axios'
+
+class Todolist extends Component {
+state = {
+    users :[]
+}
+
+componentDidMount() {
+    axios.get (`http://localhost:3002/posts`)
+        .then(res => {
+        console.log(res)
+        this.setState({users:res.data})
+    })
+}
+
+deleteRow(id, e){
+    axios.delete(`http://localhost:3002/posts/${id}`)
+      .then(res => {
+        console.log(res);
+        console.log(res.data);
+  
+        const users = this.state.users.filter(item => item.id !== id);
+        this.setState({ users });
+      })
+  
+  }
+
+
+    render() {
+        return (
+            <div>
+        <h1>Kamera Bakım İşlemleri</h1>
+  
+        <table className="table table-bordered">
+            <thead>
+              <tr>
+                  <th>ID</th>
+                  <th>Title</th>
+                  <th>Body</th>
+                  <th>Actions</th>
+              </tr>
+            </thead>
+
+            <tbody>
+                {this.state.users.map(item => (
+                <tr>
+                    <td key={item.id}>{item.id}</td>
+                    <td>{item.title}</td>
+                    <td>{item.body}</td> 
+                    <td>
+                    <button className="btn btn-danger" onClick={(e) => this.deleteRow(item.id, e)}>Sil</button>
+                    <button className="btn btn-info" >Düzenle</button>
+                    </td>
+                </tr>
+                    ))}
+                    </tbody>
+        </table>
+                   
+                
+            </div>
+        )
+    }
+}
+export default Todolist;
